b57e01e511b6b2469aea1467e70a48e7b45d369b
RajathaDH <59072510+RajathaDH@users.noreply.github.com>
2021-01-27 15:49:32 +0530

    Changed add bookmark form as a modal

--- public/main.js
+++ public/main.js
@@ -1,4 +1,10 @@
 const bookmarks = document.querySelector('.bookmarks');
+const bookmarkForm = document.getElementById('bookmarkForm');
+const bookmarkModal = document.getElementById('addBookmark');
+
+bookmarkForm.addEventListener('submit', e => {
+    e.preventDefault();
+});
 
 async function readBookmarks() {
     const result = await fetch('../bookmarks.json');
