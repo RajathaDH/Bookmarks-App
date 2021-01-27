25723eab1930a880f782db231dbddf40b141d610
RajathaDH <59072510+RajathaDH@users.noreply.github.com>
2021-01-27 23:38:10 +0530

    Added bookmarks to document

--- public/main.js
+++ public/main.js
@@ -1,4 +1,4 @@
-const bookmarks = document.querySelector('.bookmarks');
+const bookmarksDiv = document.getElementById('bookmarks');
 const bookmarkForm = document.getElementById('bookmarkForm');
 const bookmarkModal = document.getElementById('addBookmark');
 
@@ -6,12 +6,51 @@ bookmarkForm.addEventListener('submit', e => {
     e.preventDefault();
 });
 
-async function readBookmarks() {
+async function getBookmarks() {
     const result = await fetch('../bookmarks.json');
 
     const data = await result.json();
 
-    console.log(data);
+    outputBookmarksToDOM(data.bookmarks);
 }
 
-readBookmarks();
+function outputBookmarksToDOM(bookmarks) {
+    bookmarks.forEach(bookmark => {
+        const outerDiv = document.createElement('div');
+        outerDiv.classList.add('col-sm-6');
+        outerDiv.classList.add('mb-2');
+
+        const card = document.createElement('div');
+        card.className = 'card';
+
+        const cardBody = document.createElement('div');
+        cardBody.className = 'card-body';
+
+        const heading = document.createElement('h5');
+        heading.className = 'card-title';
+        heading.textContent = bookmark.name;
+
+        const para = document.createElement('p');
+        para.className = 'card-text';
+        para.textContent = bookmark.url;
+
+        const link = document.createElement('a');
+        link.classList.add('btn');
+        link.classList.add('btn-primary');
+        link.href = bookmark.url;
+        link.textContent = 'Go';
+        link.setAttribute('target', '_blank');
+
+        cardBody.appendChild(heading);
+        cardBody.appendChild(para);
+        cardBody.appendChild(link);
+
+        card.appendChild(cardBody);
+
+        outerDiv.appendChild(card);
+
+        bookmarksDiv.appendChild(outerDiv);
+    });
+}
+
+getBookmarks();
